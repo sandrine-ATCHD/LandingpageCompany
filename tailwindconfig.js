c962d14411fa1648a9225074372e1a18a6cda3ba
Sandrine-ATCHD <satchade2005@gmail.com>
2025-02-26 11:32:40 +0100

    commit

---
+++ tailwindconfig.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          'geometos-neue': ['Geometos Neue Black', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  }
+  
